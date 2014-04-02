3282291fd258c33f59ea304b4a9381ba711f9662
baillaan <andre.baillargeon@radio-canada.ca>
2014-04-02 15:08:57 -0400

    call as_view()

--- heartbeat/urls.py
+++ heartbeat/urls.py
@@ -1,3 +1,5 @@
 from django.conf.urls import patterns, url
+from heartbeat.views import Status
 
-urlpatterns = patterns('heartbeat.views', url(r'^status/$', 'status'),)
+urlpatterns = patterns('heartbeat.views',
+                       url(r'^status/$', Status.as_view(), name='status'))
