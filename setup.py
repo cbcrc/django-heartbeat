9abc74cdedce9d176f6f66aaa82a393902d4713f
baillaan <andre.baillargeon@radio-canada.ca>
2014-04-04 15:05:19 -0400

    version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-heartbeat',
-    version='0.0.3',
+    version='0.1.0',
     description='Django Heartbeat Application',
     author='Unomena',
     author_email='dev@unomena.com',
     url='http://unomena.com',
-    packages = find_packages(),
+    packages=find_packages(),
     include_package_data=True,
-    install_requires = [],
+    install_requires=[],
     zip_safe=False,
 )
